3f46fe036a5161baa1f1ebe5ccfc26e2e07397c7
gaava <nero.gava@gmail.com>
2024-09-17 20:56:37 -0300

    fix: typo

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "var.aws_region"
-    access_key = "var.aws_access_key"
-    secret_key = "var.aws_secret_key"
-    token = "var.aws_session_token"
+    region = var.aws_region
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    token = var.aws_session_token
 }
 
 terraform {
